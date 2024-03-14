b8778812fdd967293b220dc437e7e9040a1398c2
aleksess <and0941@gmail.com>
2024-03-14 07:22:18 +0100

    set map leader

--- lua/custom/plugins/configs/nvim-tree.lua
+++ lua/custom/plugins/configs/nvim-tree.lua
@@ -1,3 +1,16 @@
+local function my_on_attach(bufnr)
+  local api = require 'nvim-tree.api'
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+end
+
 return {
   'nvim-tree/nvim-tree.lua',
   version = '*',
@@ -8,4 +21,5 @@ return {
   config = function()
     require('nvim-tree').setup {}
   end,
+  on_attach = my_on_attach,
 }
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,4 +4,5 @@
 -- See the kickstart.nvim README for more information
 return {
   { import = 'custom.plugins.configs.nvim-tree' },
+  { import = 'custom.plugins.configs.love2d-nvim' },
 }
